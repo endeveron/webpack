0b056d2a3d82774fa69e050a8bab399fbd8785da
Dmitry <endeveron@gmail.com>
2021-01-29 12:01:46 +0200

    1:42:20

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,47 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
+  context: path.resolve(__dirname, 'src'),  // assign the root catalog
   mode: 'development',
-  entry: {
+  entry: {  // entry points
     main: './index.js',
     analytics: './analytics.js'
   },
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
-  },
+  },  // result boundle destination 
   resolve: {
-    extensions: ['.js', '.json', '.png'],
+    extensions: ['.js', '.json', '.png'],  // can write only filename without the file extension
     alias: {
-      '@models': path.resolve(__dirname, 'src/models'),
+      '@models': path.resolve(__dirname, 'src/models'),  // useful for shortening
       '@': path.resolve(__dirname, 'src')
+    }
+  },
+  optimization: {
+    splitChunks: {  // prevent of duplicate code 
+      chunks: 'all'
     }
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './index.html'
+      template: './index.html'  // allows use file as a template 
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),  // cleans old unuseful files 
+    new CopyWebpackPlugin({  // copy static files to dist
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')
+        }
+      ],
+    }),
   ],
   module: {
-    rules: [
+    rules: [  // loaders 
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
